e8a65f045c6114dfd3ac15aa3d50f78a1c5b23e5
Izz Noland <izz@linux.com>
2020-10-06 03:08:26 -0500

    fixed type in case in shell file

--- files/mon.sh
+++ files/mon.sh
@@ -4,7 +4,7 @@ ping -q -c 1 -W 1 google.com >/dev/null
 status=$?
 now=`date`
 if [ $status -eq 0 ]; then
-    echo -e "$now -- [INFO] -- The network is up" >> /opt/svc_intmon/InternetMonitor/logs/netMon.log
+    echo -e "$now -- [INFO] -- The network is up" >> /opt/svc_intmon/internetMonitor/logs/netMon.log
 else
     echo -e "$now -- [ALERT] -- The network is down" >> /opt/svc_intmon/internetMonitor/logs/netMon.log	    
 fi
